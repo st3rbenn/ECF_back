7fe535cd7f25740c3ec39a05ef8c323939905705
@st3rbenn <anthonincolas37000@gmail.com>
2022-02-24 01:35:15 +0100

    filter search bar casiment fini, modal mise en place il me reste plus qu'a faire le js de celle-ci

---
+++ js/modal.js
@@ -0,0 +1,30 @@
+const filterBtn = document.querySelector('#filterBtn');
+const modal = document.querySelector('#modal')
+
+filterBtn.addEventListener('click', (ev) => {
+  modal.classList.toggle('visible')
+  modal.classList.add('animate__animated', 'animate__fadeIn', 'animate__delay-.05s')
+  setTimeout(() => {modal.classList.remove('animate__animated', 'animate__fadeIn', 'animate__delay-.05s')}, 3000)
+  
+  isTrue = true;
+})
+// setTimeout(() => {
+//   document.addEventListener(
+//     "click",
+//     function(event) {
+  
+//       if (
+//         event.target.matches(".button-close-modal") ||
+//         !event.target.closest(".modal")
+//       ) {
+//         closeModal()
+//       }
+//     },
+//     false
+//   )
+  
+//   function closeModal() {
+//     modal.classList.toggle('visible')
+//     modal.classList.remove('animate__animated', 'animate__fadeIn', 'animate__delay-.05s')
+//   }
+// }, 5000)
--- js/switch.js
+++ js/switch.js
@@ -1,8 +1,12 @@
-document.getElementById('theme-switch').addEventListener('change', function(event){
-    (event.target.checked) ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
-    (event.target.checked) ? document.getElementById('label__switch').setAttribute('data-theme', 'dark') : document.getElementById('label__switch').removeAttribute('data-theme');
-    (event.target.checked) ? document.getElementById('switch__button').setAttribute('data-theme', 'dark') : document.getElementById('switch__button').removeAttribute('data-theme');
-    (event.target.checked) ? document.getElementById('filter').setAttribute('data-theme', 'dark') : document.getElementById('filter').removeAttribute('data-theme');
+document.querySelector('#theme-switch').addEventListener('change', function(event){
+    (event.target.checked) ? 
+    document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
+    (event.target.checked) ? 
+    document.querySelector('#label__switch').setAttribute('data-theme', 'dark') : document.querySelector('#label__switch').removeAttribute('data-theme');
+    (event.target.checked) ? 
+    document.querySelector('#switch__button').setAttribute('data-theme', 'dark') : document.querySelector('#switch__button').removeAttribute('data-theme');
+    (event.target.checked) ? 
+    document.querySelector('#filter').setAttribute('data-theme', 'dark') : document.querySelector('#filter').removeAttribute('data-theme');
   });
 
   window.matchMedia('(prefers-color-scheme: dark)')
