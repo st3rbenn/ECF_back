5a3a17f4b12835faae461e50f31be6366a509c67
@st3rbenn <anthonincolas37000@gmail.com>
2022-02-26 01:19:29 +0100

    Layout phone tablet pc terminée, form casiment fini aussi,responsive totalement mis en place, bouton loadMore ajouté il manque juste le marginBottom, quelque modif dans le dark modes aussi

--- js/modal.js
+++ js/modal.js
@@ -1,33 +1,38 @@
 const filterBtn = document.querySelector('#filterBtn');
 const modal = document.querySelector('#modal')
 const modalBack = document.createElement('div')
+const checkBoxModal = document.querySelector('#filter__modal__checkBox')
 let isTrue;
-filterBtn.addEventListener('click', (ev) => {
+
+function closeModal() {
   modal.classList.toggle('visible')
   modal.classList.add('animate__animated', 'animate__fadeIn', 'animate__delay-.05s')
-  modalBack.classList.add('modalback')
-  document.body.append(modalBack)
-  setTimeout(() => {modal.classList.remove('animate__animated', 'animate__fadeIn', 'animate__delay-.05s')}, 3000)
+  setTimeout(() => {modal.classList.remove('animate__animated', 'animate__fadeOut', 'animate__delay-.05s')}, 3000)
+  modalBack.classList.remove('modalback')
+  document.documentElement.style.overflow = 'initial' ;
+}
+
+function openModal() {
+  modal.classList.toggle('visible')
+    checkBoxModal.focus();
+    modal.classList.add('animate__animated', 'animate__fadeIn', 'animate__delay-.05s')
+    modalBack.classList.add('modalback')
+    document.documentElement.style.overflow = 'hidden' ;
+    document.body.append(modalBack)
+    setTimeout(() => {modal.classList.remove('animate__animated', 'animate__fadeIn', 'animate__delay-.05s')}, 3000)  
+}
+
+
+
+filterBtn.addEventListener('click', (ev) => {
+  ev.preventDefault();
+    openModal();
+
+document.addEventListener('keydown', (e) => {
+  if (e.key == 'Escape'){
+    closeModal();
+  }
+})})
+
+
   
-  isTrue = true;
-})
-// setTimeout(() => {
-//   document.addEventListener(
-//     "click",
-//     function(event) {
-  
-//       if (
-//         event.target.matches(".button-close-modal") ||
-//         !event.target.closest(".modal")
-//       ) {
-//         closeModal()
-//       }
-//     },
-//     false
-//   )
-  
-//   function closeModal() {
-//     modal.classList.toggle('visible')
-//     modal.classList.remove('animate__animated', 'animate__fadeIn', 'animate__delay-.05s')
-//   }
-// }, 5000)
--- js/switch.js
+++ js/switch.js
@@ -2,18 +2,15 @@ document.querySelector('#theme-switch').addEventListener('change', function(even
     (event.target.checked) ? 
     document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
     (event.target.checked) ? 
-    document.querySelector('#label__switch').setAttribute('data-theme', 'dark') : document.querySelector('#label__switch').removeAttribute('data-theme');
-    (event.target.checked) ? 
     document.querySelector('#switch__button').setAttribute('data-theme', 'dark') : document.querySelector('#switch__button').removeAttribute('data-theme');
     (event.target.checked) ? 
     document.querySelector('#filter').setAttribute('data-theme', 'dark') : document.querySelector('#filter').removeAttribute('data-theme');
   });
 
-  window.matchMedia('(prefers-color-scheme: dark)')
-      .addEventListener('change', event => {
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
   if (event.matches) {
     document.body.setAttribute('data-theme', 'dark');
-    document.getElementById('switch__button').getAttribute('transition', 'left', 'var', '(--switch-animation-duration)');
+    document.querySelector('#switch__button').setAttribute('transition', 'left', 'var', '(--switch-animation-duration)');
   } else {
     document.body.removeAttribute('data-theme')
   }
